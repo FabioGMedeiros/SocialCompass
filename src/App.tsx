9ec9abdda1dfdd929605432f760814e234d33b5b
FabioGMedeiros <fabio.filho2001@hotmail.com>
2023-12-05 04:31:18 -0300

    feat: uploading images used in development and other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Cadastro from './components/Cadastro/Cadastro';
+import Login from './components/Login/Login';
+import Home from './pages/Home';
+import MeuPerfil from './pages/MeuPerfil';
 
-function App() {
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/Cadastro" element={<Cadastro onRegistrationSuccess={(userData) => {}} />} />
+          <Route path="/" element={<Login onLoginSuccess={(userData) => {}} />} />
+          <Route path="/Home" element={<Home isModalOpen={isModalOpen} onToggleModal={() => setIsModalOpen(!isModalOpen)} />}/>
+          <Route path="/Profile" element={<MeuPerfil />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
